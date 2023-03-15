1af8e7362e7d2fd38d15714c6b156597d39a2e4b
TranHuy <huy.tran9510@gmail.com>
2023-03-15 16:59:37 +0700

    Tao cac Class: Actors, Film, Genre, Schedule va sua file config util

--- Cineme-Sem2/src/main/java/POJO/FilmGenre.java
+++ Cineme-Sem2/src/main/java/POJO/FilmGenre.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package POJO;
 
-/**
- *
- * @author thuhuytran
- */
+import java.util.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class FilmGenre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int fGenreID;
+    
+    @Column(nullable = false)
+    private String fGenreName;
+    
+    @ManyToMany(mappedBy = "listGenre")
+    private Set<Film> listFilm = new HashSet<>();
+
+    
+    //Constructor
+    public FilmGenre() {
+    }
+
+    public FilmGenre(int fGenreID, String fGenreName) {
+        this.fGenreID = fGenreID;
+        this.fGenreName = fGenreName;
+    }
+    //End constructor
+    /**
+     * @return the fGenreID
+     */
+    public int getfGenreID() {
+        return fGenreID;
+    }
+
+    /**
+     * @param fGenreID the fGenreID to set
+     */
+    public void setfGenreID(int fGenreID) {
+        this.fGenreID = fGenreID;
+    }
+
+    /**
+     * @return the fGenreName
+     */
+    public String getfGenreName() {
+        return fGenreName;
+    }
+
+    /**
+     * @param fGenreName the fGenreName to set
+     */
+    public void setfGenreName(String fGenreName) {
+        this.fGenreName = fGenreName;
+    }
+
+    /**
+     * @return the listFilm
+     */
+    public Set<Film> getListFilm() {
+        return listFilm;
+    }
+
+    /**
+     * @param listFilm the listFilm to set
+     */
+    public void setListFilm(Set<Film> listFilm) {
+        this.listFilm = listFilm;
+    }
+    
+    
     
 }
