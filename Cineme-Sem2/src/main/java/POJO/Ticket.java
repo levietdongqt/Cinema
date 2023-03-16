9d7bb5b3d4f3da6e188b861bd02921b6c686f894
NotNoob06 <nguyennhatminh141144@gmail.com>
2023-03-16 21:24:49 +0700

    add Ticket

---
+++ Cineme-Sem2/src/main/java/POJO/Ticket.java
@@ -0,0 +1,82 @@
+package POJO;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticketID")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "billID", referencedColumnName = "billID")
+    private Bill bill;
+
+    @ManyToOne
+    @JoinColumn(name = "scheduleID", referencedColumnName = "scheduleID")
+    private Schedule schedule;
+
+    @Column(name = "seatMap")
+    private String seatMap;
+
+    @Column(name = "status")
+    private Boolean status;
+
+
+    public Ticket() {}
+    
+    public Ticket(String id, Bill bill, Schedule schedule, String seatMap, boolean status) {
+        this.id = id;
+        this.bill = bill;
+        this.schedule = schedule;
+        this.seatMap = seatMap;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getSeatMap() {
+        return seatMap;
+    }
+
+    public void setSeatMap(String seatMap) {
+        this.seatMap = seatMap;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+    
+    
+    
+
+}
