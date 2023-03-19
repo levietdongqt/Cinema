2f1752023ca52049c0710ffad591a13c2e06ef10
levietdongqt <115526727+levietdongqt@users.noreply.github.com>
2023-03-19 18:52:43 +0700

    Thêm hàm SetFull()

--- Cineme-Sem2/src/main/java/com/group2/cineme/sem2/App.java
+++ Cineme-Sem2/src/main/java/com/group2/cineme/sem2/App.java
@@ -26,8 +26,15 @@ public class App extends Application {
         scene.setRoot(loadFXML(fxml));
     }
 
+    static void setFull(String fxml) throws IOException {
+        Stage stage = (Stage) scene.getWindow(); // Lấy đối tượng Stage hiện tại
+        Scene newScene = new Scene(loadFXML(fxml));
+        stage.setScene(newScene); // Cài đặt Scene mới vào Stage hiện tại
+        stage.setFullScreen(true);
+    }
+
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml+".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
