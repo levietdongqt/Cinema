29c0160da492a8a1ce1105369e910a5c0ce210fa
TranHuy <huy.tran9510@gmail.com>
2023-03-22 19:55:24 +0700

    Film Handler

--- Cineme-Sem2/src/main/java/DAO/FilmGenreDAO.java
+++ Cineme-Sem2/src/main/java/DAO/FilmGenreDAO.java
@@ -5,12 +5,35 @@
 package DAO;
 
 import POJO.FilmGenre;
+import Utils.AlertUtils;
+import Utils.HibernateUtils;
+import java.util.*;
 import java.util.List;
+import javafx.scene.control.Alert;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 /**
  *
  * @author thuhuytran
  */
 public class FilmGenreDAO extends GenericDAO<FilmGenre, Integer>{
-    
+    public static List<FilmGenre> getGenreAllOrByName(String kw){
+        List<FilmGenre> listGenre = new ArrayList<>();
+        try (Session session = HibernateUtils.getFACTORY().openSession()){
+            String hql = "FROM FilmGenre";
+            if(!kw.isEmpty()){
+                hql += " WHERE fGenreName like:kw";
+            }
+            Query query = session.createQuery(hql);
+            if(!kw.isEmpty()){
+                query.setParameter("kw", '%'+kw+'%');
+            }     
+            
+            listGenre = query.getResultList();
+        } catch (Exception e) {
+            AlertUtils.getAlert(e.getMessage(), Alert.AlertType.ERROR).show();
+        }       
+        return listGenre;
+    }
 }
