12e1c82e223a373877b5c2f48bf9d0e2ab76f391
phuochuynh92 <phuochuynh92@gmail.com>
2023-03-31 09:40:42 +0700

    123

---
+++ Cineme-Sem2/src/main/java/DAO/WorkSessionDAO.java
@@ -0,0 +1,21 @@
+
+package DAO;
+
+import POJO.WorkSession;
+import Utils.HibernateUtils;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javax.persistence.Query;
+import org.hibernate.Session;
+
+public class WorkSessionDAO extends GenericDAO<WorkSession, String>{
+       
+}
