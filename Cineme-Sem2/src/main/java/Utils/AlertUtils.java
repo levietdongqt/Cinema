0ede62912f403428fcc593adafbad35befca6ec8
TranHuy <huy.tran9510@gmail.com>
2023-04-21 01:57:07 +0700

    Tao billManager

--- Cineme-Sem2/src/main/java/Utils/AlertUtils.java
+++ Cineme-Sem2/src/main/java/Utils/AlertUtils.java
@@ -2,9 +2,17 @@
 package Utils;
 
 
+import POJO.Film;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.StringConverter;
 
 public class AlertUtils {
     public static Alert getAlert(String content,Alert.AlertType typeAlert){
@@ -41,4 +49,31 @@ public class AlertUtils {
         }
         return sb.toString();
     }
+    
+    //Ham chinh comboBox
+    public static void formatLocalDateTimeInComboBox(ComboBox comboBox,String pattern){
+        StringConverter<LocalDateTime> converter = new StringConverter<LocalDateTime>() {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+
+            @Override
+            public String toString(LocalDateTime dateTime) {
+                if (dateTime != null) {
+                    return formatter.format(dateTime);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDateTime fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDateTime.parse(string, formatter);
+                } else {
+                    return null;
+                }
+            }
+        };
+        comboBox.setConverter(converter);
+    }
+    
 }
